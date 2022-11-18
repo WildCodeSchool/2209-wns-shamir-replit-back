428f5a66f00755a755cadcc1f62f909e2cea8f62
cyriacinfo <cyriac.intesse@gmail.com>
2022-11-18 11:41:45 +0100

    del consolelog

--- src/services/vmService.ts
+++ src/services/vmService.ts
@@ -17,24 +17,18 @@ const executeCode = async (code: string) => {
     let consoleOutput: ConsoleOutput[] = [];
     const resultScript = new VMScript(code);
     vm.on("console.log", (...arg: string[]) => {
-      console.log(`VM stdout: ${arg.join(" ")}`);
       consoleOutput = [...consoleOutput, { type: "log", message: arg }];
     });
     vm.on("console.info", (...arg: string[]) => {
-      console.info(`VM stdout: ${arg.join(" ")}`);
       consoleOutput = [...consoleOutput, { type: "info", message: arg }];
     });
     vm.on("console.warn", (...arg: string[]) => {
-      console.warn(`VM stdout: ${arg.join(" ")}`);
       consoleOutput = [...consoleOutput, { type: "warn", message: arg }];
     });
     vm.on("console.error", (...arg: string[]) => {
-      console.error(`VM stdout: ${arg.join(" ")}`);
       consoleOutput = [...consoleOutput, { type: "error", message: arg }];
     });
     vm.run(resultScript);
-    console.log("cslOuput", consoleOutput);
-
     return consoleOutput;
   } catch (err) {
     console.log(err);
