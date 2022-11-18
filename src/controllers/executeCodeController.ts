38154d92f7ce4e0a64ee38db6f519c18c42ba8f7
cyriacinfo <cyriac.intesse@gmail.com>
2022-11-18 10:25:11 +0100

    init vm2 + controller + routes

---
+++ src/controllers/executeCodeController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import executeCode from "../services/vmService";
+
+export const executeCodeController = async (req: Request, res: Response) => {
+  console.log("truc", req);
+
+  const { code } = req.body;
+  const result = await executeCode(code);
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(500).send("Internal server error");
+  }
+};
